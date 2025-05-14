9004564e0cb86d2970a76e07119829bcd8cc0135
nikitha-2601 <rnikithanikitha505@gmail.com>
2025-05-14 23:16:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.onclick = () => li.remove();
+
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  taskInput.value = "";
+}
